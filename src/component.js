ec88144e33095a64dfd7c670df24a37179c0a429
Zhangzhenguo3352 <z3352418z>
2016-07-17 15:34:05 +0800

    webp-2

--- src/component.js
+++ src/component.js
@@ -1,11 +1,4 @@
-module.exports = function(){
-    alert('====');
-    console.log(3)
-    alert('wolail');
-    alert('wolail');
-    alert('wolail');
-    alert('wolail');
-    alert('wolail');
-    alert('wolail');
-
+export default function(){
+    alert('=====')
+    console.log('===asa====')
 }
